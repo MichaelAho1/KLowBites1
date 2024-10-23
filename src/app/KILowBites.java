d484a3a4d2c274d9c6d44a459bf7927ba2cb5dda
Donovan Cheng <donovanjcheng@gmail.com>
2024-10-23 18:27:32 -0400

    Created the app package and the main class

---
+++ src/app/KILowBites.java
@@ -0,0 +1,20 @@
+package app;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.reflect.*;
+import javax.swing.*;
+
+/**
+ * KILowBites main class. Runs the program and the main window.
+ *
+ * @author f24team3d
+ * @version 10/23/24
+ */
+public class KILowBites
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world");
+  }
+}

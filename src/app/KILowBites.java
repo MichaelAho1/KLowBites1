11a1340fd9faa88234535b27bcaf7f89bd392078
Donovan Cheng <donovanjcheng@gmail.com>
2024-10-23 20:04:14 -0400

    created main window
    action listeners and main panel not yet implemented

--- src/app/KILowBites.java
+++ src/app/KILowBites.java
@@ -11,10 +11,111 @@ import javax.swing.*;
  * @author f24team3d
  * @version 10/23/24
  */
-public class KILowBites
+public class KILowBites implements Runnable
 {
+  private String[] args;
+
+    /**
+   * The entry point of the application.
+   *
+   * @param args  The command-line arguments (ignored)
+   * @throws InterruptedException If the system is interrupted
+   * @throws InvocationTargetException If there is a problem starting the system
+   */
   public static void main(String[] args)
+      throws InterruptedException, InvocationTargetException
   {
-    System.out.println("Hello world");
+    // Perform all of the setup activities in the event dispatch thread
+    SwingUtilities.invokeAndWait(new KILowBites(args));
+  }
+
+  /**
+   * Explicit Value Constructor.
+   *
+   * @param args The command-line arguments
+   */
+  public KILowBites(final String[] args)
+  {
+    // Store the command-line arguments if needed
+    this.args = args;
+  }
+
+  /**
+   * Run method.
+   */
+  public void run()
+  {
+    // initial setup
+    JFrame frame = new JFrame("KILowBites Main Window");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JPanel contentPane = (JPanel)frame.getContentPane();
+    contentPane.setLayout(new BorderLayout());
+
+    // creates the ribbon
+    JMenuBar menuBar = new JMenuBar();
+    frame.setJMenuBar(menuBar);
+
+    // adds ribbon items
+    JMenu menu;
+    JMenuItem menuItem;
+
+    menu = new JMenu("File");
+    menuBar.add(menu);
+    menuItem = new JMenuItem("Exit");
+    menu.add(menuItem);
+    // TODO: add action listener to exit the program (for each entry)
+
+    menu = new JMenu("Edit");
+    menuBar.add(menu);
+    menuItem = new JMenuItem("Recipe");
+    menu.add(menuItem);
+    menuItem = new JMenuItem("Meal");
+    menu.add(menuItem);
+
+    menu = new JMenu("Search");
+    menuBar.add(menu);
+    menuItem = new JMenuItem("Recipes");
+    menu.add(menuItem);
+    menuItem = new JMenuItem("Meals");
+    menu.add(menuItem);
+
+    menu = new JMenu("View");
+    menuBar.add(menu);
+    menuItem = new JMenuItem("Shopping List");
+    menu.add(menuItem);
+    menuItem = new JMenuItem("Process");
+    menu.add(menuItem);
+
+    menu = new JMenu("Tools");
+    menuBar.add(menu);
+    menuItem = new JMenuItem("Calorie Calculator");
+    menu.add(menuItem);
+    menuItem = new JMenuItem("Units Converter");
+    menu.add(menuItem);
+
+    menu = new JMenu("Configure");
+    menuBar.add(menu);
+    menuItem = new JMenuItem("Preferences");
+    menu.add(menuItem);
+    menuItem = new JMenuItem("Shortcuts");
+    menu.add(menuItem);
+
+    menu = new JMenu("Help");
+    menuBar.add(menu);
+    menuItem = new JMenuItem("About");
+    menu.add(menuItem);
+    menuItem = new JMenuItem("User Guide");
+    menu.add(menuItem);
+
+
+    // Creates the logo in the main panel
+    JPanel logoPanel = new JPanel();
+
+    // Layout the GUI
+    contentPane.add(logoPanel, BorderLayout.CENTER);
+
+    frame.setSize(600, 200);
+    // frame.pack();
+    frame.setVisible(true);
   }
 }

cb71d776c27780cc5a9edcaca51d9016438b7c19
Ben Lee <lee4bs@dukes.jmu.edu>
2024-10-25 13:50:08 -0400

    Created calculation package and Foods class

--- src/gui/CalorieCalculatorWindow.java
+++ src/gui/CalorieCalculatorWindow.java
@@ -1,22 +1,17 @@
 package gui;
 
-import java.awt.AlphaComposite;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
+
+import utilities.ImageUtilities;
 
 public class CalorieCalculatorWindow extends JFrame
 {
@@ -59,7 +54,7 @@ public class CalorieCalculatorWindow extends JFrame
 
     // Create buttons with icons
     JButton calculateButton = new JButton(
-        getColoredIconAndScale("img/calculate.png", Color.GRAY, 25, 25));
+        ImageUtilities.getColoredIconAndScale("img/calculate.png", Color.GRAY, 25, 25));
     calculateButton.setToolTipText("Calculate");
     calculateButton.setEnabled(false);
     calculateButton.addActionListener(new ActionListener()
@@ -71,7 +66,8 @@ public class CalorieCalculatorWindow extends JFrame
       }
     });
 
-    JButton resetButton = new JButton(getColoredIconAndScale("img/reset.png", Color.GRAY, 25, 25));
+    JButton resetButton = new JButton(
+        ImageUtilities.getColoredIconAndScale("img/reset.png", Color.GRAY, 25, 25));
     resetButton.setToolTipText("Reset");
     resetButton.setEnabled(false);
     resetButton.addActionListener(new ActionListener()
@@ -94,36 +90,6 @@ public class CalorieCalculatorWindow extends JFrame
     setResizable(false);
     setLocationRelativeTo(null);
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-  }
-
-  private static ImageIcon getColoredIconAndScale(String path, Color color, int width, int height)
-  {
-    try
-    {
-      // Load the original image
-      BufferedImage original = ImageIO.read(new File(path));
-
-      // Create a colored version of the original image
-      BufferedImage colored = new BufferedImage(original.getWidth(), original.getHeight(),
-          BufferedImage.TYPE_INT_ARGB);
-      Graphics2D g = colored.createGraphics();
-      g.drawImage(original, 0, 0, null);
-      g.setComposite(AlphaComposite.SrcIn);
-      g.setColor(color);
-      g.fillRect(0, 0, original.getWidth(), original.getHeight());
-      g.dispose();
-
-      // Scale the colored image
-      Image scaledImage = colored.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-
-      // Return the scaled image wrapped in an ImageIcon
-      return new ImageIcon(scaledImage);
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-      return null;
-    }
   }
 
   @Override

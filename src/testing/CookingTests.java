e1f76281920bbfdb7fbcc3fe031998c059c8bf12
Albede3 <boycedal@dukes.jmu.edu>
2024-10-31 20:40:43 -0400

    Ingredients tests

--- src/testing/CookingTests.java
+++ src/testing/CookingTests.java
@@ -1,5 +1,53 @@
 package testing;
 
-public class CookingTests {
-	
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import cooking.Ingredients;
+
+
+/**
+ * Test cases for Cooking package.
+ * 
+ * @author f24team3d
+ * @version 10/28/24
+ */
+
+class CookingTests
+{
+  @Test
+  void testIngredients()
+  {
+	  Ingredients ing = new Ingredients();
+	  assertEquals(0.0, ing.getAmount());
+	  assertEquals("", ing.getUnit());
+	  assertEquals("", ing.getDetails());
+	  assertEquals("", ing.getName());
+  }
+  @Test
+  void testIngredients1()
+  {
+	  Ingredients ing = new Ingredients(3, "individual", "(sliced ripe)", "bananas");
+	  assertEquals(3.0, ing.getAmount());
+	  assertEquals("individual", ing.getUnit());
+	  assertEquals("(sliced ripe)", ing.getDetails());
+	  assertEquals("bananas", ing.getName());
+  }
+  @Test
+  void testIngredients2()
+  {
+	  Ingredients ing = new Ingredients();
+	  ing.setAmount(3.0);
+	  ing.setUnit("individual");
+	  ing.setDetails("(sliced ripe)");
+	  ing.setName("bananas");
+	  assertEquals(3.0, ing.getAmount());
+	  assertEquals("individual", ing.getUnit());
+	  assertEquals("(sliced ripe)", ing.getDetails());
+	  assertEquals("bananas", ing.getName());
+	  
+	  assertEquals("3.0 individual (sliced ripe) bananas", ing.toString());
+	  ing.add();
+  }
 }

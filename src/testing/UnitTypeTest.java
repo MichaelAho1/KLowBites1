55657bec51c7bac688020eea102e632ff650767a
MichaelAho1 <ahoma@dukes.jmu.edu>
2024-11-14 19:20:04 -0500

    Add Tests for Unit Type

---
+++ src/testing/UnitTypeTest.java
@@ -0,0 +1,29 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import utilities.UnitType;
+
+class UnitTypeTest
+{
+  @Test
+  void test()
+  {
+    String[] imperialUnits = UnitType.getImperialUnits();
+    String[] imperialUnitsSpace = UnitType.getImperialUnitsSpace();
+    String[] imperialUnitsIndividual = UnitType.getImperialUnitsIndividual();
+    String[] metricUnits = UnitType.getMetricUnits();
+    String[] metricUnitsIndividual = UnitType.getMetricUnitsSpace();
+    String[] metricUnitsSpace = UnitType.getMetricUnitsIndividual();
+    
+    UnitType.setImperialSelected();
+    assertEquals(UnitType.getImperialSelected(), true);
+    assertEquals(UnitType.getMetricSelected(), false);
+    UnitType.setMetricSelected();
+    assertEquals(UnitType.getImperialSelected(), false);
+    assertEquals(UnitType.getMetricSelected(), true);
+  }
+
+}

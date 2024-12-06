da59c636caadd470ff81c2b3dcfe88fe2e3ecb21
Albede3 <boycedal@dukes.jmu.edu>
2024-12-06 12:51:45 -0500

    Checkstyle

--- src/controller/PrinterController.java
+++ src/controller/PrinterController.java
@@ -2,25 +2,33 @@ package controller;
 
 import java.awt.print.*;
 import javax.swing.*;
- 
+
+/**
+ * PrinterController Class.
+ */
 public class PrinterController
 {
-    public static void print(Printable printable, JFrame parent)
+  /**
+   * Print.
+   * 
+   * @param printable
+   * @param parent
+   */
+  public static void print(final Printable printable, final JFrame parent)
+  {
+    PrinterJob job = PrinterJob.getPrinterJob();
+    try
     {
-        PrinterJob job = PrinterJob.getPrinterJob();
-        try
-        {
-            job.setPrintable(printable);
-            boolean shouldPrint = job.printDialog();
-            if (shouldPrint) {
-            	job.print();
-            }
-        }
-        catch (Exception e)
-        {
-            JOptionPane.showMessageDialog(parent, 
-                                          "Unable to print!",
-                                          "Error", JOptionPane.ERROR_MESSAGE);
-        }
+      job.setPrintable(printable);
+      boolean shouldPrint = job.printDialog();
+      if (shouldPrint)
+      {
+        job.print();
+      }
     }
+    catch (Exception e)
+    {
+      JOptionPane.showMessageDialog(parent, "Unable to print!", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+  }
 }

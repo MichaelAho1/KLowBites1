9d71c74cfe6d99580f8262c65ad72c6a82e43d97
Donovan Cheng <donovanjcheng@gmail.com>
2024-12-11 09:29:42 -0500

    Add javadoc

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"controller"},{"l":"converter"},{"l":"cooking"},{"l":"gui"},{"l":"testing"},{"l":"utilities"}];updateSearchResults();
